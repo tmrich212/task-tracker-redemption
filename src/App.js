abee337da3f9f74a71d10763df0707bd1857f818
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-05-01 17:00:54 -0400

    Delete function and mapping of hard coded data updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TaskPanel from './components/TaskPanel';
 
 function App() {
+  const [tasks, setNewTasks] = useState([
+    {title: 'Nothing to see here', status: 'incomplete', id: 1},
+    {title: 'Just testing the waters', status: 'in progress', id: 2},
+  ]);
+
+  const handleClick = (id) => {
+    setNewTasks((prevTask) => {
+      return prevTask.filter((tasks) => {
+        return id !== tasks.id;
+      })
+    })
+  }
+
   return (
     <div className="App">
       <Form />
-      <TaskPanel />
+      <TaskPanel tasks={tasks} handleClick={handleClick}/>
     </div>
   );
 }
--- src/components/TaskPanel.js
+++ src/components/TaskPanel.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './TaskPanel.css'
 
-const TaskPanel = () => {
+const TaskPanel = ({ tasks, handleClick }) => {
     return ( 
         <div className='task-panel'> 
-            <h1>Your Todo List</h1>
+        <header><h1>Your Todo List</h1></header>
+            <div className='task-container'>
+            {tasks.map((task) => (
+                  <div key={task.id} className="task-item">
+                    <h3>{task.title}</h3>
+                    <p>status: {task.status}</p>
+                    <p>due date: {task.date}</p>
+                    <button onClick={() => handleClick(task.id)}>delete item</button>
+                  </div>   
+            ))}
+            </div>
         </div>
      );
 }
