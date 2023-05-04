39537ad11b1f6935cf43ebd58d3608dd0ec6ddd5
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-05-04 15:14:42 -0400

    Task panel changed to map through data as table for better styling

--- src/components/TaskPanel.js
+++ src/components/TaskPanel.js
@@ -4,17 +4,41 @@ import './TaskPanel.css'
 const TaskPanel = ({ tasks, handleClick }) => {
     return ( 
         <div className='task-panel'> 
-        <header><h1>Your Todo List</h1></header>
+           <header><h1>Your To-do List</h1></header>
+
+          <table className='table'>
+            <thead>
+              <tr>
+                <th className='expand'>Task</th>
+                <th>Status</th>
+                <th>Due Date</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task, idx) => (
+                <tr key={task.idx}>
+                  <td>{task.task}</td>
+                  <td>{task.status}</td>
+                  <td>{task.date}</td>
+                  <td><button onClick={() => handleClick(task.id)}>delete item</button></td>
+                </tr>
+              ))}
+
+            </tbody>
+          </table>
+
+{/*        
             <div className='task-container'>
             {tasks.map((task) => (
                   <div key={task.id} className="task-item">
                     <h3>{task.task}</h3>
                     <p>status: {task.status}</p>
                     <p>due date: {task.date}</p>
-                    <button onClick={() => handleClick(task.id)}>delete item</button>
+                    
                   </div>   
             ))}
-            </div>
+            </div> */}
         </div>
      );
 }
